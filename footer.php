a902a190579c858b3137eb60134b2cb0c96f02a3
Doruk Ozalp <doruk.ozalp@gmail.com>
2011-07-18 00:26:52 +0300

    php files removed

--- footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="span-23 last box">
-	<p><strong>Copyleft <?=date("Y")?></strong></p>
-	<a href="http://<?=$_SERVER["HTTP_HOST"]?>"><?=$_SERVER["HTTP_HOST"]?></a>
-</footer>
-</section>
-<script type="text/javascript" src="js/customSelectBox.js"></script>
-<script type="text/javascript" src="js/index.js"></script>
-</body>
-</html> 
